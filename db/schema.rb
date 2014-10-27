25017b7090e7e7f63921dc667838a11ab9b35b76
Xiaoming Zhang <xmming.1988@gmail.com>
2014-10-27 14:11:20 -0500

    create microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027162415) do
+ActiveRecord::Schema.define(version: 20141027185411) do
+
+  create_table "microposts", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
